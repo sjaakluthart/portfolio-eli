2e9e7b5b3ea8cd7dd9269b6f6e5af23038a552dc
Sjaak Luthart <sjaak.luthart@anchor.chat>
2017-07-12 20:14:57 +0200

    Added photosets.getInfo to get createdAt.

--- src/actions/sets.js
+++ src/actions/sets.js
@@ -12,32 +12,39 @@ const setsGetSuccess = payload => ({
 
 const setsGet = (collectionId, sets) => (
   dispatch => (
-    each(sets, setId => (
-      axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${settings.apiKey}&photoset_id=${setId}&user_id=${settings.userId}&extras=url_l,description&format=json&nojsoncallback=1`)
+    each(sets, (setId) => {
+      const album = {};
+      axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getInfo&api_key=${settings.apiKey}&photoset_id=${setId}&user_id=${settings.userId}&format=json&nojsoncallback=1`)
       .then((res) => {
         if (res.status === 200 && res.data.stat === 'ok') {
-          const photos = map(res.data.photoset.photo, photo => ({
-            src: photo.url_l,
-            width: parseInt(photo.width_l, 10),
-            height: parseInt(photo.height_l, 10),
-            caption: `${photo.title} - ${photo.description && photo.description._content}`
-          }));
+          album.createdAt = new Date(res.data.photoset.date_create * 1000);
 
-          const album = {
-            title: res.data.photoset.title,
-            photos
-          };
+          axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${settings.apiKey}&photoset_id=${setId}&user_id=${settings.userId}&extras=url_l,description&format=json&nojsoncallback=1`)
+          .then((photosRes) => {
+            if (photosRes.status === 200 && photosRes.data.stat === 'ok') {
+              const photos = map(photosRes.data.photoset.photo, photo => ({
+                src: photo.url_l,
+                width: parseInt(photo.width_l, 10),
+                height: parseInt(photo.height_l, 10),
+                caption: `${photo.title} - ${photo.description && photo.description._content}`
+              }));
 
-          dispatch(setsGetSuccess({
-            collectionId,
-            album
-          }));
+              album.title = photosRes.data.photoset.title;
+              album.photos = photos;
+
+              dispatch(setsGetSuccess({
+                collectionId,
+                album
+              }));
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
         }
       })
-      .catch((err) => {
-        console.log(err);
-      })
-    ))
+      .catch(err => console.log(err));
+    })
   )
 );
 
